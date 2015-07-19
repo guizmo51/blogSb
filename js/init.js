12398c5d625d7a9f16e8a1a689c31199c33b7452
Simon BLUM <simon51100@gmail.com>
2015-07-19 22:42:39 +0200

    First commit

---
+++ js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.slider').slider();
+     $('.scrollspy').scrollSpy();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
